ba2baa4b0ee4e3d05b41d33da869d64874e119bc
david <dross78375@gmail.com>
2018-07-22 22:34:31 +0100

    add_torrents test

--- test/test_autoTransmission.sh
+++ test/test_autoTransmission.sh
@@ -1,4 +1,8 @@
-. ../autoTransmission.sh --torrent_dir ${HOME}/Downloads/
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p ${HERE}/temp/
+. ../autoTransmission.sh --torrent_dir ${HERE}/temp/
+# HERE changes to parent directory
+TEMP="${HERE}/test/temp/"
 
 test_startup() {
 	# ensure startup results in transmission-daemon to be active
@@ -6,5 +10,24 @@ test_startup() {
 	status=$(systemctl is-active transmission-daemon.service)
 	assertEquals "active" $status
 }
+
+test_add_torrents() {
+	wget http://releases.ubuntu.com/18.04/ubuntu-18.04-desktop-amd64.iso.torrent -P $TEMP
+	add_torrents
+	torrent_name="ubuntu-18.04-desktop-amd64.iso"
+	torrent_id_list=$(transmission-remote  -l | sed -e '1d;$d;s/^ *//' | \
+			          cut --only-delimited --delimiter \  --fields 1)
+	torrent_id=$(echo $torrent_id_list | awk '{print $NF}')
+	name=$(transmission-remote -t $torrent_id --info | grep -o $torrent_name)
+	assertEquals $torrent_name $name
+	cleanup
+}
+
+
+cleanup() {
+	rm -r $TEMP
+	transmission-remote -t $torrent_id --remove-and-delete
+}
+
 
 . shunit2
